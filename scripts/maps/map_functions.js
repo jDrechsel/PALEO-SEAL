ad91d17c1606ef4144b4501b9527cb52a9500543
jDrechsel <jpmdrechsel@gmail.com>
2020-06-10 14:44:45 +0200

    map sidenav table & plotmarker:export

--- scripts/maps/map_functions.js
+++ scripts/maps/map_functions.js
@@ -78,6 +78,7 @@ expandControllerMAPfunc = function($scope, $filter, leafletData){
             layer: "samples",
             xUID: id.proben[i].xUID,
             indicatorType: id.proben[i].type_string,
+            reference: id.proben[i].reference,
             // message: <h3>id.proben[i].label</h3> + "Age: " + id.proben[i].age + "; Elevation: " +id.proben[i].elevation + "; Indicator Type: " + id.proben[i].type + "; Ref: "+ id.proben[i].reference
             message: '<h4>'+ id.proben[i].label +'</h4>'+  id.proben[i].reference + '<p>' + id.proben[i].type_string  + '</p><p> Age:'  + id.proben[i].age + '- Elevation:'  +id.proben[i].elevation + '</p>' 
             };

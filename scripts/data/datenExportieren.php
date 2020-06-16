4f93f721213895676a0c1afde14a230ab917c886
jDrechsel <jpmdrechsel@gmail.com>
2020-06-16 15:35:19 +0200

    Update datenExportieren.php

--- scripts/data/datenExportieren.php
+++ scripts/data/datenExportieren.php
@@ -1,58 +1,26 @@
 <?php 
-
-
-// header('Cache-Control: max-age=0');
-// echo "\n PHP -> \n php: export routine started 1 \n -> POST: ";
-// $entityBody = file_get_contents('php://input');
-// print_r($entityBody);
-// print_r($_POST);
-// echo "php: export routine started 2 \n -> etwas: ";
-$etwas=json_decode($_POST['data']);
-// var_dump($etwas);
-// $etwas2=$_POST['data'];
-// echo "php: export routine started 3 \n -> REQUEST: ";
-// var_dump($_REQUEST);
-// echo "php: export routine started 4 \n";
 require 'connect.php' ; // load $con
-// require 'connect_LOCAL.php' ; // load $con
-//require 'loadCustomData.php'
-//loadCustomData();
-// $exportUIDs=$_POST['data'];
-// $exUIDs='('. implode(',',$exportUIDs) .')';
-// echo $exportUIDs;
-// print_r($exportUIDs);
 
-function createInString($something){
+$exportList=json_decode($_POST['data']);
+
+function createInString($listEntry){
     $tmp=array();
-    // foreach($something as $item){
-    //     $tmp[] = mysqli_escape_string($con, $item);
-    // }
-    // return '\'' .implode('\',\'', $tmp ) . '\'';
-    return '(\'' .implode('\',\'', $something ) . '\')';
+
+    return '(\'' .implode('\',\'', $listEntry ) . '\')';
 };
-$etwas2=createInString($etwas);
+$exportString=createInString($exportList);
 
-// echo "php: export conversion started 5 \n -> etwas2: ";
-// var_dump($etwas2);
-
-$sql="SELECT * FROM holsea_dataworkbook_40 WHERE A_00 IN $etwas2";
+// retreive column headers from db
+$sql="SELECT * FROM holsea_dataworkbook_40 WHERE A_00 IN $exportString";
 $headersSQL="SHOW COLUMNS FROM data_entries_newdata";
 $headersQuery=mysqli_query($con, $headersSQL);
-// $headersResults = $headersQuery -> fetch_all();
 $headersResults = array();
 while ($row = $headersQuery->fetch_assoc()){
-    // var_dump($row["Field"]);
     $headersResults[] = $row["Field"];
 };
 
-function getHeaderStrings($var){
-    // var_dump($var[0]);
-    return $var[0];
-}
- $headers = array_map( "getHeaderStrings", $headersResults);
 
-//new query with uncertainties
-//$sql=options(MYSQLI_OPT_INT_AND_FLOAT_NATIVE, 1);
+
 $d=mysqli_query($con,$sql)
     or die("Error: ".mysqli_error($con));
 
@@ -62,9 +30,8 @@ $d=mysqli_query($con,$sql)
         $ProbenKurz[] = $row;
     }
 
-// $ProbenKurz=mysqli_fetch_all($d);
 
-// print json_encode($ProbenKurz);
+
 $retreived= json_encode($ProbenKurz);
 
 $first=true;
@@ -74,44 +41,25 @@ if (empty($retreived)) {
   }
 
 $filename = "exportfile" .date("Y-m-d_H_i_s") . ".csv";
-// $filename = "exportfile";
-// $out = fopen('data/report.csv', 'w');
+
 $handle = fopen($filename, 'w+');
-// $handle = fopen('php://output', 'w+');
-// fputcsv($handle, $headers);
-// echo $headers;
+
 fputcsv($handle, $headersResults);
 
 foreach ($ProbenKurz as $line){
-    // var_dump($line);
     fputcsv($handle, $line);
 }
 
 fclose($handle);
-// rewind($handle);
-// header('Content-Type: text/csv; charset=utf-8');
-// header('Content-Disposition: attachment; filename=\'$filename\'');
+
 header('Content-Description: File Transfer');
 header('Content-Type: application/octet-stream');
 header('Content-Disposition: attachment; filename="$filename"');
-// header('Content-Disposition: attachment; filename='.basename($filename));
 header('Content-Transfer-Encoding: binary');
 header('Expires: 0');
 header('Cache-Control: must-revalidate');
 header('Pragma: public');
 header('Content-Length: ' . filesize($filename));
 readfile($filename);
-// fpassthru( $filename);
-// while($row = mysqli_fetch_all($d,MYSQLI_BOTH)){
-//     if($first){
-//         $titles = array();
-//         foreach($row as $key=>$val){
-//             $titles[] = $key;
-//         }
-//         fputcsv($out, $titles);
-//         $first = false;
-//     }
-//     fputcsv($out, $row);
-// }
 
  ?>

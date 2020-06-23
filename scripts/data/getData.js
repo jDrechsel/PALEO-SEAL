935b67c5b42525158d442ecd5b0e6d8468e9a5cf
jDrechsel <jpmdrechsel@gmail.com>
2020-06-23 18:07:43 +0200

    plot elevation errors update

--- scripts/data/getData.js
+++ scripts/data/getData.js
@@ -16,13 +16,9 @@
                 // Generate Array from loaded Data 
                 
                 var  werteALL =[];
-                // console.log($scope.proben, $scope.proben.length);
                 
-                // Iterate through list of samples and generate the necessary array
+                // Iterate through list of samples and generate array
                 for (var i = 0; i < $scope.proben.length; i++){
-                    // console.log("iterated stuff:", $scope.proben[i], i )
-                    // Replace String 'modern' with value zero (plotting)
-                    // var age_temp = $scope.proben[i].C_02 == "Modern" ? 0 : parseFloat($scope.proben[i].C_02);
 
                     /*replace wrong entries */
                     // find records where C_02='modern' and replace with float 0;
@@ -31,29 +27,22 @@
                     } else {
                         var age_temp = parseFloat($scope.proben[i].C_02);
                     }
-                    // Get RSL for SL indicators and Sample Elevation for MArine & Terrestrial Limiting 
-                    if ($scope.proben[i].D5_01 === 0) {
+                    // Get RSL(D7_01) for SL indicators and Sample Elevation(D3_11) for Marine & Terrestrial Limiting 
+                    if ($scope.proben[i].D5_01 == 0) {
                         var elevation_temp = parseFloat($scope.proben[i].D7_01);
-                        let elevation_errorPlus = parseFloat($scope.proben[i].D7_02);
-                        let elevation_errorMinus = parseFloat($scope.proben[i].D7_03);
+                        var elevation_errorPlus = parseFloat($scope.proben[i].D7_02);
+                        var elevation_errorMinus = parseFloat($scope.proben[i].D7_03);
                     } else {
                         var elevation_temp = parseFloat($scope.proben[i].D3_11);
-                        let elevation_errorPlus = parseFloat($scope.proben[i].D3_13);
-                        let elevation_errorMinus = parseFloat($scope.proben[i].D3_14);
+                        var elevation_errorPlus = parseFloat($scope.proben[i].D3_13);
+                        var elevation_errorMinus = parseFloat($scope.proben[i].D3_14);
                     }
-                    // replace wrong coordinates
-                    if ($scope.proben[i].B_02 == "Modern") {
 
-                    } else {
+                    // extract publication year from Reference via RegEx
+                    var regExpYear=/[0-9]+/;
+                    var match1 = parseInt(regExpYear.exec($scope.proben[i].A_02)[0]);
 
-                    }
-                    // extract publication year from Reference
-                    var regExpYear=/\(([^]+)\)/;
-                    var regExpYear2=/[0-9]+/;
-
-                    var match1 = parseInt(regExpYear2.exec($scope.proben[i].A_02)[0]);
-                    var match2 = $scope.proben[i].A_02.match(regExpYear);
-                    // Push them into werteAll
+                    // Push entry into werteAll
                     werteALL.push({
                         xUID: parseInt($scope.proben[i].A_00),
                         label: $scope.proben[i].A_01,
@@ -61,6 +50,8 @@
                         type_string: nachSchlagen(parseInt($scope.proben[i].D5_01), 'Type'),
                         age: age_temp,
                         elevation: elevation_temp,
+                        elevationMinus: elevation_errorMinus,
+                        elevationPlus: elevation_errorPlus,
                         lat: parseFloat($scope.proben[i].B_03),
                         lng: parseFloat($scope.proben[i].B_04),
                         reference: $scope.proben[i].A_02.trim(),
@@ -73,39 +64,18 @@
                         ageErrorPlus: parseFloat($scope.proben[i].C_05),
                         ageErrorMinus: parseFloat($scope.proben[i].C_06),
                         ageError: parseFloat($scope.proben[i].C_03),
-                        sElevationErrorPlus: parseFloat($scope.proben[i].D3_13),
-                        sElevationErrorMinus: parseFloat($scope.proben[i].D3_14),
                         export: false,
                         map: false,
                         plot: false,
                         myFocus: false,
-                        plotmDx: parseFloat($scope.proben[i].C_03),
-                        plotmX1: parseFloat($scope.proben[i].C_06), 
-                        plotmX2: parseFloat($scope.proben[i].C_05),
-                        plotmY1: parseFloat($scope.proben[i].D3_14), // distinguish marker type -> 
-                        plotmY2: parseFloat($scope.proben[i].D3_13) // I: [y1=p-err, y2=p+err]; M: [y1=p, y2=p+2err]; T: [y1=p-2err, y2=p]
-
+                      
                     })
                 };
                 $scope.werteALL=werteALL;
-                // console.log("end", $scope.werteALL);
+                // initialize with Region selected
                 $scope.groupBy( 'region', 'Region' );
 
             })
         }; //end of $scope.getProben
 
-        // $scope.getFullRecords=function(selection){
-        //     $http({
-        //         method: "POST",
-        //         data: selection,
-        //         url: "scripts/data/datenExportieren.php"
-        //     }).then(function mySuccess(response){
-        //         $scope.probenExport = response.data;
-        //         console.log("Daten fuer Export geladen...",response.data);
-        //     }, function myError(response){
-        //         console.log("Daten NICHT geladen!",response);
-
-        //     });
-        // }
-        
     } //end of expand
